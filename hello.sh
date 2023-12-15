f63dc52689e682d9ac5299459a9c058740d130aa
rajesh chanti <Rajeshchanti>
2023-12-15 10:02:55 +0530

    func calling

--- hello.sh
+++ hello.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "Enter Username:"
-read -s USER_NAME
-echo "Enter Password:"
-read -s PASSWORD
-
 VALIDATE(){
-    if [ $USER_NAME -e admin ]
+    if [ $USER_NAME -e 'admin' ]
     then
         echo "Wellcome to Tech Tree $USER_NAME"
     else
         echo "Invalid credentials"
     fi
 }
+
+echo "Enter Username:"
+read -s USER_NAME
+echo "Enter Password:"
+read -s PASSWORD
+
+VALIDATE
