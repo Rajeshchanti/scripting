c13df202586f22ff1b1d447ffe70b0a03b6e4f67
rajesh chanti <Rajeshchanti>
2023-12-17 18:01:03 +0530

    evenodd

--- installing.sh
+++ installing.sh
@@ -3,15 +3,22 @@
 ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executed at $TIMESTAP" &>> LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2.......installation is failed"
+        echo "$2.......installation is $R failed $N"
         exit 1
     else
-        echo "$2.......Installation is Success"
+        echo "$2.......Installation is $G Success $N"
     fi
 }
 
