60f96c0328570e81f0e8e016edfdedecbaf4e528
rajesh chanti <Rajeshchanti>
2023-12-17 17:09:51 +0530

    evenodd

---
+++ installing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if [ $? -ne 0]
+    then
+        echo "installation is failed"
+        exit 1
+    else
+        echo "Installation is Success"
+}
+
+    if [ $ID -ne 0]
+    then
+        echo "You are not root"
+        exit 1
+    else
+        echo "You are a root"
+    fi
+
+yum install git -y
