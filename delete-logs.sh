2eebd3bf82a4ecd41b6a9e1fca16aafb991fe985
rajesh chanti <Rajeshchanti>
2023-12-20 18:38:19 +0530

    adding more files

---
+++ delete-logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SOURCE_DIR="/tmp/script.logs"
+if [ ! -d $SOURCE_DIR ]
+then
+    echo -e "$R No such $SOURCE_DIR directory found $N"
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") #finding old files
+while IFS=read -r line
+do
+    echo "Deleting file:$line"
+    rm -rf $line
+done <<< FILES_TO_DELETE
