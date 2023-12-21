81caae02b3f1c7f1112c306b6063339d0b2a7bd1
rajesh chanti <Rajeshchanti>
2023-12-21 09:51:01 +0530

    archive|delete

--- delete-logs.sh
+++ delete-logs.sh
@@ -14,5 +14,5 @@ FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") #finding ol
 while IFS= read -r line
 do
     echo "Deleting file:$line"
-    rm -r $line
+    rm -rd $line
 done <<< FILES_TO_DELETE
