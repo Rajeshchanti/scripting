ef9d34f346033b7c40e3bbbc87dc7d3631c0dd06
rajesh chanti <Rajeshchanti>
2023-12-21 09:41:00 +0530

    archive|delete

--- delete-logs.sh
+++ delete-logs.sh
@@ -3,14 +3,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SOURCE_DIR="/tmp/script.logs"
+SOURCE_DIR="/tmp/script/logs.log"
 if [ ! -d $SOURCE_DIR ]
 then
     echo -e "$R No such $SOURCE_DIR directory found $N"
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") #finding old files
-while IFS=read -r line
+
+while IFS = read -r line
 do
     echo "Deleting file:$line"
     rm -rf $line
